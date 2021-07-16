1fc7fa44a1ee81b19ad1357fb6b1007bd03adbe6
BradlyLewis <bradlylewisdev@gmail.com>
2021-07-16 19:58:45 -0400

    update mvp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import Character from "./components/Character"
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")
+    .then((response) => {
+      setCharacters(response.data)
+      console.log(response.data)
+    })
+    .catch(errors => console.log(errors))
+  },[]);
+
   // sync up with, if any.
 
   return (
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,2 +1,24 @@
 // Write your Character component here
+import React from "react"
+import styled from "styled-components"
 
+const StyledCharacter = styled.div`
+    background-color: #debd9288;
+    padding: 0.25em 2em;
+    margin: 2em auto;
+    width: 90%;
+    text-align: left;
+    border: 2px solid #e4a654;
+`;
+
+const Character = (props) => {
+    const {character} = props;
+
+    return (
+        <StyledCharacter>
+            <h1>{character.name}</h1>
+        </StyledCharacter>
+    )
+};
+
+export default Character;
