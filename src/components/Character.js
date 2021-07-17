2bbd0ca5f55fb4dd054fb4d039cb78da61d447c7
BradlyLewis <bradlylewisdev@gmail.com>
2021-07-16 22:22:25 -0400

    update mvp

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,27 @@
 // Write your Character component here
 import React from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
-// const StyledContainer = styled.
+const StyledContainer = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+border: 3px solid;
+padding: 2%;
+margin: 0 10%;
+
+h2{
+    font-family: 'Courier New', sans-serif;
+}
+`
 
 export default function Character(props){
     const {data} = props
 
-    return data.map(item => <div><h2>{item.name}</h2><p>{item.birth_year}</p></div>)
+    return data.map(item => (
+    <StyledContainer>
+        <h2>{item.name}</h2>
+        <p>{item.birth_year}</p>
+    </StyledContainer>)
+    )
 }
